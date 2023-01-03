f9ecb582060f838b4cdcc989d7af01d48f8cce8e
Kitazuka <ilya.artnaev@gmail.com>
2023-01-03 13:35:45 +0200

    Create Check and Printer models and register it in admin panel

--- check/admin.py
+++ check/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from check.models import Check, Printer
+
+
+@admin.register(Printer)
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = ["name", "api_key", "check_type", "point_id"]
+    list_filter = ["check_type"]
+
+
+@admin.register(Check)
+class CheckAdmin(admin.ModelAdmin):
+    list_display = ["id", "printer_id", "type", "status"]
+    list_filter = ["printer_id", "type", "status"]
+
--- check/models.py
+++ check/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class TypeChoices(models.TextChoices):
+    CLIENT = "Client"
+    KITCHEN = "Kitchen"
+
+
+class Printer(models.Model):
+    name = models.CharField(max_length=50)
+    api_key = models.CharField(max_length=255, primary_key=True)
+    check_type = models.CharField(max_length=50, choices=TypeChoices.choices)
+    point_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Check(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        NEW = "new"
+        RENDERED = "rendered"
+        PRINTED = "printed"
+
+    printer_id = models.ForeignKey(
+        Printer, on_delete=models.CASCADE, related_name="checks"
+    )
+    type = models.CharField(max_length=50, choices=TypeChoices.choices)
+    order = models.JSONField()
+    status = models.CharField(
+        max_length=50, choices=StatusChoices.choices, default="new"
+    )
+    pdf_file = models.FileField(upload_to="media/pdf", null=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.status}"
